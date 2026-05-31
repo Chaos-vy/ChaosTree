889249cb922326b64e0213392e7e19f1dcec1e91
chaos-vy <vy.singh.24@gmail.com>
2026-05-31 20:34:15 +0530

    test: update Main to verify BST, AVL, RBT toString output

--- src/chaos/tree/Main.java
+++ src/chaos/tree/Main.java
@@ -1,37 +1,31 @@
 package chaos.tree;
+ import chaos.tree.binary.AVL;
  import chaos.tree.binary.BST;
+ import chaos.tree.binary.RBT;
  import chaos.tree.core.ITree;
 
  import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
-        BST<Integer> bst = new BST<>();
-        bst.insert(45);
-        bst.insert(52);
-        bst.insert(23);
-        bst.insert(50);
-        bst.insert(30);
-        bst.insert(70);
-        bst.insert(20);
-        bst.insert(40);
-        bst.insert(60);
-        bst.insert(80);
-
-        System.out.println(bst.search(23));
-        System.out.println("Is the value present = "+bst.search(40));
-
-        System.out.println(bst.height());
-
-        System.out.println("Size = "+bst.size());
-        List<Integer> list = bst.preorder();
-        System.out.println(list);
-
-        bst.insertAll(List.of(112,150,152,145,298,352));
-        System.out.println(bst.inorder());
-
-        ITree<Integer> bst1 = new BST<>();
-        bst1.insertAll(List.of(112,150,152,145,298,352));
-        System.out.println(bst1.inorder());
+        ITree<Integer> bst = new BST<>();
+        ITree<Integer> avl = new AVL<>();
+        ITree<Integer> rbt = new RBT<>();
+        for (int i = 1; i <= 20; i++) {
+            bst.insert(i);
+            avl.insert(i);
+            rbt.insert(i);
+        }
+        System.out.println(bst);
+        System.out.println(avl);
+        System.out.println(rbt);
+        System.out.println(rbt.size());
+        rbt.delete(5);
+        avl.delete(5);
+        bst.delete(5);
+        System.out.println(rbt.size());
+        System.out.println(avl.size());
+        System.out.println(bst.size());
     }
 }
