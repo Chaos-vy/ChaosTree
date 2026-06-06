fbc8778920082ac0883f43680484b471a2988370
Chaos-vy <vy.singh.24@gmail.com>
2026-06-06 16:22:51 +0530

    Feat: Add SplayNode implementation

---
+++ src/main/java/chaos/tree/binary/splay/SplayNode.java
@@ -0,0 +1,18 @@
+package chaos.tree.binary.splay;
+
+import chaos.tree.core.binary.node.ParentBiNode;
+
+/**
+ * Splay tree  node structure
+ * @param <T> value type to be stored in node
+ */
+public class SplayNode<T extends Comparable<T>> extends ParentBiNode<T, SplayNode<T>> {
+    /**
+     * Constructs a Splay Tree node with the specified value.
+     *
+     * @param value the value to be stored in this node
+     */
+    public SplayNode(T value) {
+        super(value);
+    }
+}
