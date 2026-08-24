849f1d03d8f48d4eeeedf110efd6b948291aaa15
Chaos-vy <vy.singh.24@gmail.com>
2026-08-24 09:59:33 +0530

    build: improved the performance and Java docs

---
+++ ct-core/src/main/java/chaos/tree/binary/BSTNode.java
@@ -0,0 +1,26 @@
+package chaos.tree.binary;
+
+import chaos.tree.core.searchtree.binary.node.BiNode;
+
+/**
+ * Concrete node representation for the Binary Search Tree.
+ *
+ * <p>This node is utilized specifically by the {@link BST} class to construct
+ * its tree structure. It extends {@link BiNode} to inherit left and right child pointers
+ * as well as the value storage.</p>
+ *
+ * @param <T> the type of value stored in the node
+ * @see BST
+ * @see BiNode
+ * @since 1.0.0
+ */
+public class BSTNode<T extends Comparable<? super T>> extends BiNode<T, BSTNode<T>> {
+    /**
+     * Constructs a new BST node with the specified value.
+     *
+     * @param value the value to be stored in the node, which will be managed by {@link BST}
+     */
+    BSTNode(T value) {
+        super(value);
+    }
+}
---
+++ ct-core/src/main/java/chaos/tree/binary/RBTNode.java
@@ -0,0 +1,61 @@
+package chaos.tree.binary;
+
+import chaos.tree.core.searchtree.binary.node.ParentBiNode;
+
+import static chaos.tree.binary.Color.RED;
+
+/**
+ * Represents a node in a Red-Black Tree ({@link RBT}).
+ *
+ * <p>Alongside the value, parent, and child references inherited from {@link ParentBiNode}, this
+ * node stores a {@link Color} attribute (either {@link Color#RED} or {@link Color#BLACK}).
+ * The Red-Black Tree uses this color state to maintain structural balance after insertions
+ * and deletions. Newly constructed nodes are initialized with {@link Color#RED} by default.</p>
+ *
+ * @param <T> the type of value stored in this node; must be {@link Comparable}
+ * @see RBT
+ * @see ParentBiNode
+ * @see Color
+ * @since 1.0.0
+ */
+public class RBTNode<T extends Comparable<? super T>> extends ParentBiNode<T, RBTNode<T>> {
+
+    /**
+     * Color of this node; newly created nodes are RED by default.
+     */
+    private Color color;
+
+
+    /**
+     * Constructs a Red-Black Tree node with the specified value.
+     *
+     * <p>Newly created nodes are initialized with {@link Color#RED} by default,
+     * as required by the Red-Black Tree insertion algorithm.</p>
+     *
+     * @param value the value to store in this node
+     */
+    RBTNode(T value) {
+        super(value);
+        this.color = RED;
+    }
+
+
+    /**
+     * Returns the current color of this node.
+     *
+     * @return the node color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * Sets the current color of this node.
+     *
+     * @param color the new color
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+}
