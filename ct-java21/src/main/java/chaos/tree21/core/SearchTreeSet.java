8c90f151605a05382f81d846e245fbf8ac6a0ffc
Chaos-vy <vy.singh.24@gmail.com>
2026-08-27 17:58:12 +0530

    minor: fixed the broken tree printer. Changed signature from print() -> display()

--- ct-java21/src/main/java/chaos/tree21/core/SearchTreeSet.java
+++ ct-java21/src/main/java/chaos/tree21/core/SearchTreeSet.java
@@ -2,7 +2,6 @@ package chaos.tree21.core;
 
 import chaos.tree21.binary.AbstractBinaryTreeSet;
 import chaos.tree21.nary.AbstractNaryTreeSet;
-//import chaos.tree21.nary.AbstractNaryTreeSet;
 
 import java.util.NavigableSet;
 import java.util.SequencedSet;
@@ -11,5 +10,9 @@ public sealed interface SearchTreeSet<E> extends NavigableSet<E>, SequencedSet<E
         permits AbstractBinaryTreeSet, AbstractNaryTreeSet {
     int height();
 
-    String print();
+    default String display(){
+        return display(Style.ASCII);
+    }
+
+    public String display(Style style);
 }
