6d46490dbaf2dd7378869843a40c9e01c3155dfd
Chaos-vy <vy.singh.24@gmail.com>
2026-08-28 22:14:29 +0530

    minor: Strictly follow up behaviour of TreeSet

--- ct-java21/src/main/java/chaos/tree21/binaryMap/AvlMapNode.java
+++ ct-java21/src/main/java/chaos/tree21/binaryMap/AvlMapNode.java
@@ -1,6 +1,6 @@
 package chaos.tree21.binaryMap;
 
-final class AvlMapNode<K, V> extends AbstractBinaryMapNode<K, V, AvlMapNode<K, V>> {
+public final class AvlMapNode<K, V> extends AbstractBinaryMapNode<K, V, AvlMapNode<K, V>> {
     private int height;
 
     AvlMapNode(K key, V value) {
