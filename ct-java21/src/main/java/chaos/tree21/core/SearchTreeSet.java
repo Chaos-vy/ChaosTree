0dbc796f19b3b57af4cd86aa033342f4ad7c94be
Chaos-vy <vy.singh.24@gmail.com>
2026-08-31 21:12:01 +0530

    public interfaces and enum class

--- ct-java21/src/main/java/chaos/tree21/core/SearchTreeSet.java
+++ ct-java21/src/main/java/chaos/tree21/core/SearchTreeSet.java
@@ -1,18 +1,14 @@
 package chaos.tree21.core;
 
-import chaos.tree21.binary.AbstractBinaryTreeSet;
-import chaos.tree21.nary.AbstractNaryTreeSet;
-
 import java.util.NavigableSet;
 import java.util.SequencedSet;
 
-public sealed interface SearchTreeSet<E> extends NavigableSet<E>, SequencedSet<E>
-        permits AbstractBinaryTreeSet, AbstractNaryTreeSet {
-    int height();
+public interface SearchTreeSet<E> extends NavigableSet<E>, SequencedSet<E> {
 
-    default String display(){
+    default String display() {
         return display(Style.ASCII);
     }
 
-    public String display(Style style);
+    String display(Style style);
+
 }
