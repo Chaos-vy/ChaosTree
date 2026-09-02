92c22c495e8bfe3f7cadfa95b13948ade8ce69ba
Chaos-vy <vy.singh.24@gmail.com>
2026-09-02 20:51:15 +0530

    major: Making node structure Clean So that it does not bloat with any library

--- ct-java21/src/main/java/chaos/tree21/naryMap/AbstractNaryMapNode.java
+++ ct-java21/src/main/java/chaos/tree21/naryMap/AbstractNaryMapNode.java
@@ -1,11 +1,8 @@
 package chaos.tree21.naryMap;
 
-import java.util.Map;
-import java.util.Objects;
 
 sealed abstract class AbstractNaryMapNode<K, V, N extends AbstractNaryMapNode<K, V, N>>
         permits BTreeMapNode, BPlusTreeMapNode {
-    //First problem how do I hold the chaos of Map.Entry???
     protected final Object[] keys;
     protected final Object[] values;
     protected final N[] child;
@@ -19,56 +16,6 @@ sealed abstract class AbstractNaryMapNode<K, V, N extends AbstractNaryMapNode<K,
         this.values = needsValues ? new Object[maxKeys] : null;
         this.child = child;
         this.keyCount = 0;
-    }
-
-    // Hold ChaosEntry in game!!
-    static final class ChaosEntry<K, V> implements Map.Entry<K, V> {
-        private final AbstractNaryMapNode<K, V, ?> node;
-        private final int index;
-
-        ChaosEntry(AbstractNaryMapNode<K, V, ?> node, int index) {
-            this.node = node;
-            this.index = index;
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public K getKey() {
-            return (K) node.keys[index];
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public V getValue() {
-            return (V) node.values[index];
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public V setValue(V newValue) {
-            V oldValue = (V) node.values[index];
-            node.values[index] = newValue;
-            return oldValue;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Map.Entry<?, ?> e)) return false;
-            return Objects.equals(getKey(), e.getKey()) && Objects.equals(getValue(), e.getValue());
-        }
-
-        @Override
-        public int hashCode() {
-            K k = getKey();
-            V v = getValue();
-            return (k == null ? 0 : k.hashCode()) ^ (v == null ? 0 : v.hashCode());
-        }
-
-        @Override
-        public String toString() {
-            return getKey() + "=" + getValue();
-        }
     }
 
     @SuppressWarnings("unchecked")
