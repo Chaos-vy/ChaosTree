4283ec9e776ce13d266ba3b6b192862e8d070082
Chaos-vy <vy.singh.24@gmail.com>
2026-09-20 13:10:36 +0530

    test based on TreeSet/Map compliance

---
+++ chaos-tree/src/test/java/chaos/tree/binary/BinarySetApiTest.java
@@ -0,0 +1,63 @@
+package chaos.tree.binary;
+
+import chaos.tree.AbstractNavigableSetApiTest;
+import net.jqwik.api.Example;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+
+public class BinarySetApiTest extends AbstractNavigableSetApiTest {
+
+    @Property(tries = 10000)
+    void rbtMatchesTreeSet(
+            @ForAll("initialLoad") List<Integer> initial,
+            @ForAll("actions") List<Action> actions) {
+        runScenario(new RedBlackTreeSet<>(), initial, actions);
+    }
+
+    @Property(tries = 10000)
+    void avlMatchesTreeSet(
+            @ForAll("initialLoad") List<Integer> initial,
+            @ForAll("actions") List<Action> actions) {
+        runScenario(new AvlTreeSet<>(), initial, actions);
+    }
+
+    @Example
+    void rbtEdgeCases() {
+        edgeCases(RedBlackTreeSet::new);
+    }
+
+    @Example
+    void avlEdgeCases() {
+        edgeCases(AvlTreeSet::new);
+    }
+
+    @Example
+    void rbtFailFastIterators() {
+        failFast(RedBlackTreeSet::new);
+    }
+
+    @Example
+    void avlFailFastIterators() {
+        failFast(AvlTreeSet::new);
+    }
+
+    @Property(tries = 100)
+    void testCloneAndDisplay(@ForAll @IntRange(min = 50,max = 100) int size) {
+        AvlTreeSet<Integer> tree1 = new AvlTreeSet<>();
+        for (int i = 0; i < size; i++) {
+            tree1.add(i);
+        }
+        Assertions.assertNotNull(tree1.display());
+        Assertions.assertEquals(tree1, tree1.clone());
+        RedBlackTreeSet<Integer> tree2 = new RedBlackTreeSet<>();
+        for (int i = 0; i < size; i++) {
+            tree2.add(i);
+        }
+        Assertions.assertNotNull(tree2.display());
+        Assertions.assertEquals(tree2, tree2.clone());
+    }
+}
---
+++ chaos-tree/src/test/java/chaos/tree/binaryMap/BinaryMapApiTest.java
@@ -0,0 +1,73 @@
+package chaos.tree.binaryMap;
+
+import chaos.tree.AbstractNavigableMapApiTest;
+import net.jqwik.api.Example;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+
+public class BinaryMapApiTest extends AbstractNavigableMapApiTest {
+
+    @Property(tries = 10000)
+    void rbtMatchesTreeMap(
+            @ForAll("initialLoad") List<Integer> initial,
+            @ForAll("actions") List<Action> actions) {
+        runScenario(new RedBlackTreeMap<>(), initial, actions);
+    }
+
+    @Property(tries = 10000)
+    void avlMatchesTreeMap(
+            @ForAll("initialLoad") List<Integer> initial,
+            @ForAll("actions") List<Action> actions) {
+        runScenario(new AvlTreeMap<>(), initial, actions);
+    }
+
+    @Example
+    void rbtEdgeCases() {
+        edgeCases(RedBlackTreeMap::new);
+    }
+
+    @Example
+    void avlEdgeCases() {
+        edgeCases(AvlTreeMap::new);
+    }
+
+    @Example
+    void rbtFailFastIterators() {
+        failFast(RedBlackTreeMap::new);
+    }
+
+    @Example
+    void avlFailFastIterators() {
+        failFast(AvlTreeMap::new);
+    }
+
+    @Example
+    void rbtFunctionsThatMutateTheMap() {
+        mutatingFunctions(RedBlackTreeMap::new);
+    }
+
+    @Example
+    void avlFunctionsThatMutateTheMap() {
+        mutatingFunctions(AvlTreeMap::new);
+    }
+
+    @Property(tries = 100)
+    void testCloneAndDisplay(@ForAll @IntRange(min = 50,max = 100) int size) {
+        AvlTreeMap<Integer,Integer> tree1 = new AvlTreeMap<>();
+        for (int i = 0; i < size; i++) {
+            tree1.put(i,i);
+        }
+        Assertions.assertNotNull(tree1.display());
+        Assertions.assertEquals(tree1, tree1.clone());
+        RedBlackTreeMap<Integer,Integer> tree2 = new RedBlackTreeMap<>();
+        for (int i = 0; i < size; i++) {
+            tree2.put(i,i);
+        }
+        Assertions.assertNotNull(tree2.display());
+        Assertions.assertEquals(tree2, tree2.clone());
+    }
+}
